b9669cf9151fee11b47048890171f5161159c34c
i.lesnikov <i.lesnikov@bars-group.ru>
2023-12-17 20:56:25 +0300

    Delete commentary

--- src/test/java/guru/qa/BaseTest.java
+++ src/test/java/guru/qa/BaseTest.java
@@ -11,7 +11,7 @@ abstract public class BaseTest {
     public static void setUp() {
         Configuration.browserSize = "1600х900";
         Configuration.pageLoadStrategy = "eager";
-        /*
+
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -20,6 +20,6 @@ abstract public class BaseTest {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
-         */
+         
     }
  }
